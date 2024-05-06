4af10f8f8e198606550f335c34b9fe54c0e3b236
MirShukhman <miriysha@gmail.com>
2024-05-06 11:09:53 +0300

    deploy

--- events-front-end/src/App.js
+++ events-front-end/src/App.js
@@ -84,7 +84,7 @@ function App() {
           <Routes>
 
             {/* Unlogged */}
-            <Route path="/" element={<LandingPage onLogIn={openLogIn} />} />
+            <Route exact path="/" element={<LandingPage onLogIn={openLogIn} />} />
             <Route path="/search_event" element={<SearchEvent />} />
             <Route path="/view_event/:event_id" element={<ViewEvent onLogIn={openLogIn} />} />
             <Route path="/user/:user_id" element={<ViewUser />} />
